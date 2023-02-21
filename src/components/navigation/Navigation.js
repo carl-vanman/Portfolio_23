5dcfc44ddb6cec1f5c679056c4e56949ac09d336
Carl Vänman <70966062+carl-vanman@users.noreply.github.com>
2023-02-21 21:24:13 +0100

    expand menu

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -8,11 +8,14 @@ const Navigation = () => {
     const handleOpenClose = () => setIsMenuOpen(prev => prev = !prev)
 
   return (
-    <nav>
+    <nav className={styles.nav}>
         <button className={styles.menu_btn} onClick={() => handleOpenClose()}>
             <div className={`${isMenuOpen ? styles.close_state : styles.open_state}`} />
             <div className={`${isMenuOpen ? styles.close_state : styles.open_state}`} />
         </button>
+        <ul className={`${styles.menu} ${isMenuOpen ? styles.open : styles.close}`}>
+           
+        </ul>
     </nav>
   )
 }
