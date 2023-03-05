9402991ae37a38bf2d1b7aafac7a858797c0b145
Carl Vänman <70966062+carl-vanman@users.noreply.github.com>
2023-03-05 20:32:40 +0100

    sliced image with css instead of static image

--- src/components/art/Sliced.js
+++ src/components/art/Sliced.js
@@ -5,7 +5,9 @@ import Section from '../section/Section'
 
 import styles from "./Sliced.module.css"
 
-import sliced from "../../../public/images/art/sliced.png";
+import eyes from "../../../public/images/art/eyes.png";
+
+const divs = [1,2,3,4,5,6,7,8,9,10,11]
 
 const Sliced = () => {
   return (
@@ -14,10 +16,13 @@ const Sliced = () => {
             <Image
                 className={styles.art_image}
                 alt="Eyes close-up with text lucky printed on forhead"
-                src={sliced}
+                src={eyes}
                 fill
                 sizes="100%"
             />
+            <div className={styles.lines_container}>
+                {divs.map( div =>  <div key={div}/>)}
+            </div>
         </div>
     </Section>
   )
