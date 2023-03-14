a83a95238f17e5594e0eb0b015d347967a51ec62
Carl Vänman <70966062+carl-vanman@users.noreply.github.com>
2023-03-14 23:15:12 +0100

    made some finalterations to Indroduction component. have made smaller components of logic and top and bottom headers and cleaning up the css module

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -1,93 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Section from "../section/Section";
 
+import useScrollPosition from "@/hooks/useScrollPosition";
+
 import styles from "./Introduction.module.css";
 import utils from "../../styles/utils.module.css";
+import IntroductionHeaders from "./IntroductionHeaders";
 
 const Introduction = () => {
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollPosition(position);
-    };
+    const [scrollPosition] = useScrollPosition();
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll, { passive: true });
+    const exitTranslateDuration = 0.1
+    const exitTranslate = scrollPosition * exitTranslateDuration
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    const initOpacity = 1
+    const exitOpacityDuration = 0.0015
+    const exitOpacity = initOpacity - scrollPosition * exitOpacityDuration
 
     return (
         <Section>
-            <div className={styles.container}>
-                <h2
-                    className={`${utils.xxl_heading} ${styles.top_title}`}
-                    style={{
-                        transform: `translateX(-${scrollPosition * 0.1}px)`,
-                        opacity: `${1 - scrollPosition * 0.0015}`,
-                    }}
-                >
-                    Creative
-                </h2>
-                <article className={`${styles.about_container}`}>
-                    <h3 className={`${utils.lg_heading}`}>
-                        Innovative
-                    </h3>
-                    <div className={`${styles.marquee}`}>
-                        <ul className={styles.marquee_content}>
-                            <li>
-                                <h4 className={`${utils.md_heading_light}`}>
-                                    Blending Technical Knowledge and Creative Skills to
-                                    Create Innovative Solutions{" "}
-                                </h4>
-                            </li>
-                        </ul>
-                        <ul className={styles.marquee_content} aria-hidden="true">
-                            <li>
-                                <h4 className={`${utils.md_heading_light}`}>
-                                    Blending Technical Knowledge and Creative Skills to
-                                    Create Innovative Solutions{" "}
-                                </h4>
-                            </li>
-                        </ul>
-                    </div>
-                    <p className={`${utils.body_text}`}>
-                        My name is Carl, a humble and kind person with a
-                        passion for creativity and technology. I love blending my technical knowledge and
-                        creative skills into my work.
-                    </p>
-                    <aside
-                        className={`${utils.tag_text}`}
-                    >{`[ Introduction ]`}</aside>
-                </article>
-                <div
-                    className={`${styles.bottom_title}`}
-                >
-                    <h2 className={`${utils.xxl_heading}`}
+            <div className={styles.introduction_wrapper}>
+                <IntroductionHeaders>
+                    <article
+                        className={`${styles.about_container}`}
                         style={{
-                            transform: `translateX(${scrollPosition * 0.05}px)`,
-                            opacity: `${1 - scrollPosition * 0.0015}`,
+                            transform: `translateY(${exitTranslate}px)`,
+                            opacity: `${exitOpacity}`,
                         }}
                     >
-                        Fro<span className={`${utils.xxl_heading} ${styles.text_outlined}`}>nt</span>
-                    </h2>
-                    <h2 className={`${utils.xxl_heading} ${styles.text_outlined}`}
-                    style={{
-                        transform: `translateX(${scrollPosition * 0.15}px)`,
-                        opacity: `${1 - scrollPosition * 0.0015}`,
-                    }}
-                    >
-                        <span
-                            className={`${utils.xxl_heading} ${styles.text_outlined}`}
-                        >
-                            {"- "}
-                        </span>
-                        End
-                    </h2>
-                </div>   
+                        <h3 className={`${utils.lg_heading}`}>Innovative</h3>
+                        <div className={styles.rotatingTextCarousel}>
+                            <h4
+                                className={`${utils.md_heading_light} ${styles.dummy}`}
+                            >
+                                Dummy
+                            </h4>
+
+                            <h4
+                                className={`${utils.md_heading_light} ${styles.side_front}`}
+                            >
+                                Blending Technical
+                            </h4>
+                            <h4
+                                className={`${utils.md_heading_light} ${styles.side_bottom}`}
+                            >
+                                Knowledge and Creativity
+                            </h4>
+                            <h4
+                                className={`${utils.md_heading_light} ${styles.side_back}`}
+                            >
+                                to Formulate
+                            </h4>
+                            <h4
+                                className={`${utils.md_heading_light} ${styles.side_top}`}
+                            >
+                                Beautiful Solutions.
+                            </h4>
+                        </div>
+                        <p className={`${utils.body_text}`}>
+                            My name is Carl, a humble and kind person with a
+                            passion for creativity and technology. I love
+                            blending my technical knowledge and creative skills
+                            into my work.
+                        </p>
+                        <aside className={`${utils.tag_text}`}>
+                            {`[ Introduction ]`}
+                        </aside>
+                    </article>
+                </IntroductionHeaders>
             </div>
         </Section>
     );
